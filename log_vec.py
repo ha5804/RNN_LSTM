b621953841bd50cdf3361e1cf001ed7d92ec62d2
ha5804 <zixpotf5803@gmail.com>
2025-07-12 16:29:26 +0900

    log file class write code

--- log_vec.py
+++ log_vec.py
@@ -1,32 +1,39 @@
 from drain3 import TemplateMiner
 from drain3.template_miner_config import TemplateMinerConfig
-
-config = TemplateMinerConfig()
-
-template_miner = TemplateMiner(config = config)
-
-event_ids = []
-
-with open("./data/HDFS_2k.log", "r") as f:
-    for line in f:
-        result = template_miner.add_log_message(line.strip())
-        event_id = result["cluster_id"]
-        event_ids.append(event_id)
-
 from sklearn.preprocessing import LabelEncoder
-
-encoder = LabelEncoder()
-event_ids_encoded = encoder.fit_transform(event_ids)
-
 import numpy as np
 
-seq_len = 10 
-X = []
-y = []
+class log_temp:
+    def __init__(self, path = "./data/HDFS_2k.log" ,seq_len = 10):
+        self.config = TemplateMinerConfig()
+        self.template_miner = TemplateMiner(config = self.config)
+        self.seq_len = seq_len
+        self.path = path
+        self.event_ids_encoded = None
+        pass
+        
+    def get_event_ids(self):
+        event_ids = []
 
-for i in range(len(event_ids_encoded) - seq_len):
-    X.append(event_ids_encoded[i:i+seq_len])   
-    y.append(event_ids_encoded[i+seq_len])     
+        with open(self.path, "r") as f:
+            for line in f:
+                result = self.template_miner.add_log_message(line.strip())
+                event_id = result["cluster_id"]
+                event_ids.append(event_id)
+        return event_ids
+    
+    def encode_event_ids(self):
+        encoder = LabelEncoder()
+        self.event_ids_encoded = encoder.fit_transform(self.get_event_ids())
+    
+    def make_seq_list(self):
+        X = []
+        y = []
 
-X = np.array(X)  
-y = np.array(y) 
+        for i in range(len(self.event_ids_encoded) - self.seq_len):
+            X.append(self.event_ids_encoded[i:i+self.seq_len])   
+            y.append(self.event_ids_encoded[i+self.seq_len])     
+
+        X = np.array(X)  
+        y = np.array(y) 
+        return X , y
