4e399965041b2c5a697589e8b9ba48d15c22059b
ha5804 <zixpotf5803@gmail.com>
2025-07-12 11:09:46 +0900

    definition forget gate

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RNN_LSTM
+
+### Element
+
+- x_t : 현재 시점 입력
---
+++ mycode.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class MyModel:
+    def __init__(self):
+
+        pass
+
+    def forget_gate(self):
+        
