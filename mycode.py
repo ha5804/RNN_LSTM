835047bd9a3cac0c1f1cb2bedc091ee82b1a1f12
ha5804 <zixpotf5803@gmail.com>
2025-07-12 11:15:29 +0900

    declare concat_vector

--- mycode.py
+++ mycode.py
@@ -7,6 +7,11 @@ class MyModel:
         self.w_f = w_f
         self.b_f = b_f
         pass
-
-    def forget_gate(self):
+    
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def forget_gate(self, h_prev, x_t):
+        concat_vector = np.vstack([h_prev, x_t])
         
+        f_t = self.sigmoid()
