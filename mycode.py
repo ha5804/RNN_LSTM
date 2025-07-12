8f5316d3d7d14a576368fd70fbc21779851d3547
ha5804 <zixpotf5803@gmail.com>
2025-07-12 11:20:50 +0900

    change frequently used code to return self

--- mycode.py
+++ mycode.py
@@ -6,16 +6,20 @@ class MyModel:
 
         self.w_f = w_f
         self.b_f = b_f
+        self.concat_vector = None
         pass
     
+    def get_concat_vector(self, h_prev, x_t):
+        self.concat_vector = np.vstack([h_prev, x_t])
+        
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
     
-    def forget_gate(self, h_prev, x_t):
-        concat_vector = np.vstack([h_prev, x_t])
-        value = (self.w_f @ concat_vector) + self.b_f
+    def forget_gate(self):
+        value = (self.w_f @ self.concat_vector) + self.b_f
         f_t = self.sigmoid(value)
         return f_t
     
     def input_gate(self):
-        
+        self.concat_vector
+
