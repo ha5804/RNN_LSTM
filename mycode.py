3d59cef08a249e085d06993cae0cd8bd05ab3150
ha5804 <zixpotf5803@gmail.com>
2025-07-12 11:10:47 +0900

    set of weigt value and bias

--- mycode.py
+++ mycode.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class MyModel:
-    def __init__(self):
+    def __init__(self, w_f, b_f):
 
+        self.w_f = w_f
+        self.b_f = b_f
         pass
 
     def forget_gate(self):
