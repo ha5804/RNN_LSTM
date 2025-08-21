d972d1bd96f9f6708b58fdc3d55b32a28d5da123
ha5804 <zixpotf5803@gmail.com>
2025-08-21 17:20:31 +0900

    explain

--- data_process.py
+++ data_process.py
@@ -14,7 +14,7 @@ class DataProcess:
     
     def log_label(self):
         label = []
-        with open(self.path, "r") as f:
+        with open(self.path, "r") as f: #로그 한줄씩 읽고 특정 키워드면 1 or 0
             for line in f:
                 if any(keyword in line for keyword in ['WARN', 'ERROR', "FATAL"]):
                     label.append(1)
@@ -31,12 +31,14 @@ class DataProcess:
                 event_id = result["cluster_id"]
                 event_ids.append(event_id)
         return event_ids
-    
+    #드레인3로 로그 패턴화, 같은 종류의 로그끼리 묶음
+
     def encode_event_ids(self):
         encoder = LabelEncoder()
         self.event_ids_encoded = encoder.fit_transform(self.get_event_ids())
         return self.event_ids_encoded
-    
+    #clust id는 문자열이므로 정수로 변환
+
     def make_train_seq_list(self):
         x = []
         y = []
@@ -53,7 +55,8 @@ class DataProcess:
         x = np.array(x)  
         y = np.array(y) 
         return x , y
-    
+    #학습용 트레이닝 데이터 만들기. autoencoder에서는 정상 데이터로만 학습
+
     def make_test_seq_list(self):
         x = []
         y = []
@@ -69,7 +72,7 @@ class DataProcess:
         x = np.array(x)  
         y = np.array(y) 
         return x , y
-    
+    #테스트셋은 정상 이상 모두 포함.
     
     #debug==============================================
     def count_warning_sequences(self, labels, seq_len = 10):
@@ -91,3 +94,5 @@ class Embedding:
     
     def forward(self, input_seq):
         return self.embedding_matrix[input_seq] 
+#데이터를 대입하기위해 데이터를 저차원 벡터로 변환 event 갯수가 행으로, 차원은 우리가 설정.
+#event - size = 3, embedding_dim = 5로 설정시, 3,5의 행렬 생성
